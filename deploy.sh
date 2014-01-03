6a3446e15718a422206e6a8b236353fe1ae42fe1
icarus4 <icarus4.chu@gmail.com>
2014-01-03 17:05:39 +0800

    deploy.sh v0.0.2

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,16 @@
 #!/bin/sh
 
-VERSION="0.0.1"
+VERSION="0.0.2"
 TOOLS_DIR="$( cd "$( dirname $0 )" && pwd -P )"
 DOTFILE_DIR="$TOOLS_DIR/dotfiles"
+SCRIPT_DIR="$TOOLS_DIR/install_scripts"
 BACKUP_DIR="$TOOLS_DIR/backup/`date +"%b-%d-%Y_%H:%M:%S"`"
 mkdir -p "$BACKUP_DIR"
 
-# functions
-red() {
-	echo -e "\033[31m${1}\033[0m"
-}
-green() {
-	echo -e "\033[32m${1}\033[0m"
-}
-yellow() {
-	echo -e "\033[33m${1}\033[0m"
-}
+COMMON_DEF="$SCRIPT_DIR/common_def"
+
+# source common functions & variables
+[[ -f $COMMON_DEF ]] && . $COMMON_DEF || ( echo "Cannot find common define fine, exit" && exit 1 )
 
 # Install dot files
 cd $DOTFILE_DIR
@@ -26,7 +21,7 @@ rcfiles=`ls *`
 cd $HOME
 for file in $rcfiles; do
 	if [ -f "$file" ] || [ -L "$file" ]; then
-		yellow "$HOME/$file is existed"
+		_yellow "$HOME/$file is existed"
 		echo "1) Skip"
 		echo "2) Overwrite"
 		echo "3) backup and replace"
@@ -35,18 +30,18 @@ for file in $rcfiles; do
 
 		case $choice in
 		1)
-			green "Skip!"
+			_green "Skip!"
 			;;	
 		2)
 			#rm $file
 			mv $file $BACKUP_DIR/
 			ln -s $DOTFILE_DIR/$file $file
-			green "Creating symbolic link ${file} ....OK"
+			_green "Creating symbolic link ${file} ....OK"
 			;;
 		3)
 			mv $file $BACKUP_DIR/
 			ln -s $DOTFILE_DIR/$file $file
-			green "Creating symbolic link ${file} ....OK"
+			_green "Creating symbolic link ${file} ....OK"
 			;;
 		*)
 			;;
@@ -57,17 +52,5 @@ for file in $rcfiles; do
 done
 
 # Finish message
-echo ""
-yellow " ______________________________"
-yellow "/ You are using icarus4.tools \\ "
-yellow "\ be productive!              /"
-yellow " -----------------------------"
-yellow "        \\   ^__^    v$VERSION "
-yellow "         \\  (oo)\\_______"
-yellow "            (__)\\       )\\/\\ "
-yellow "                ||----w |"
-yellow "                ||     ||"
-green  "         vv  v            vv"
-green  "     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
-echo -e "\n"
+_print_finish_message
 
